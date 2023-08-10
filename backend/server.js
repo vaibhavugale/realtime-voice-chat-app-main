879069aa38aa77057acba45e69f7b6c6d476d16e
vaibhavugale <ugalevaibhav00@gmail.com>
2023-08-10 11:48:38 +0530

    env

--- backend/server.js
+++ backend/server.js
@@ -8,16 +8,17 @@ const cookiesParser = require("cookie-parser");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: `${process.env.BASE_URL}`,
     methods: ["GET", "POST"],
   },
 });
+console.log(process.env.BASE_URL)
 
 app.use(cookiesParser());
 
 const corsOption = {
   credentials: true,
-  origin: ["http://localhost:3000"],
+  origin: [`${process.env.BASE_URL}`],
 };
 app.use(cors(corsOption));
 app.use("/storage", express.static("storage"));
--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -43,7 +43,7 @@ api.interceptors.response.use(
     if (err.response.status == 401 && err.config && !err.config._isRetry) {
       originalRequest.isRetry = true;
       try {
-        const response = await axios.get(
+        const response = await api.get(
           `${process.env.REACT_APP_API_URL}/api/refresh`,
           {
             withCredentials: true,
